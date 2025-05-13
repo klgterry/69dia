ca72dd364e0ab73d46debeba3f516fc286708f0d
klgterry <klgterry@gmail.com>
2025-05-13 14:48:39 +0900

    59차커밋(Footer 수정)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,9 @@
 // components/Footer.jsx
 export default function Footer() {
-    return (
-        <footer className="w-full bg-slate-900 border-t border-slate-800 py-4 text-center text-sm text-gray-400">
-  © {new Date().getFullYear()} 디아블로2 69 내전기록실. 모든 문의는 702702 01 240826 국민 조민형 입금 후 가능.
-</footer>
-
-
-      
-    );
-  }
-  
+  return (
+    <footer className="w-full bg-slate-900 border-t border-slate-800 py-4 text-center text-sm text-gray-400 leading-relaxed">
+      © {new Date().getFullYear()} 디아블로2 69 내전기록실 <br />
+      Developer 마인드, QA & Planner 린스, Design 블핑, 울프, Investment 민형
+    </footer>
+  );
+}
